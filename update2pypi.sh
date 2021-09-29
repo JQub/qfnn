ec2b310b7c5cfeab7a4c020bb0ae71eee0476594
weiwenjiang <jiang.wwen@gmail.com>
2021-09-29 12:59:18 -0400

    auto update

--- update2pypi.sh
+++ update2pypi.sh
@@ -1,6 +1,18 @@
+VERSION=`cat setup.cfg  | grep '^version =' | sed 's/version = 0.0.//'`
+NEW_VERSION=`expr $VERSION + 1`
+sed -i "s/version = 0.0.$VERSION/version = 0.0.$NEW_VERSION/g" setup.cfg
+
 python -m build
 twine upload dist/*
+echo "Wait for updating qfnn"
+
+sleep 2
+echo "Start to update qfnn"
 pip install qfnn -U
+
+
+echo "Wait for updating API document"
+sleep 2
 cd doc
 make clean
 make html
