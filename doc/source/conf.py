d66b65efcd5c47cf0dd398349d7ca704f6eb3cda
weiwenjiang <jiang.wwen@gmail.com>
2021-10-16 23:26:59 -0400

    manu

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, JQub'
 author = 'Zhirui Hu & Weiwen Jiang'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.12'
+release = '0.1.17'
 
 
 # -- General configuration ---------------------------------------------------
